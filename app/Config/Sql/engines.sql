139ea4676cf923a86450da3b2bd2564404b4c7c9
Chad Engler <englercj@live.com>
2012-08-05 17:29:01 -0400

    Converted .dat files into .sql files

---
+++ app/Config/Sql/engines.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `engines`
+--
+
+LOCK TABLES `engines` WRITE;
+
+INSERT INTO `engines` VALUES (1,'Source',''),(2,'Goldsource',''),(3,'Unreal',''),(4,'Quake',''),(5,'Unkown','');
+
+UNLOCK TABLES;
