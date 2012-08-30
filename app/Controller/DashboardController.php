f17fcc5028d384e447f47a01a51a79d9a85f352d
Chad Engler <englercj@live.com>
2012-08-30 10:44:17 -0400

    Added DashboardController as 'home'

---
+++ app/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppController', 'Controller');
+
+class DashboardController extends AppController {
+    
+    function beforeFilter() {
+        parent::beforeFilter();
+    }
+    
+    public function index() {
+        
+    }
+}
+?>
