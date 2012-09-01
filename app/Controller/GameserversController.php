9b499ce4a9b3ec40c4be5181955b979d266b0a4e
Chad Engler <englercj@live.com>
2012-09-01 07:40:57 -0400

    Stubbed out Gameservers

---
+++ app/Controller/GameserversController.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppController', 'Controller');
+
+class GameserversController extends AppController {
+}
+
+?>
---
+++ app/Model/Gameservers.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppModel', 'Model');
+
+class Model extends AppModel {
+}
+
+?>
